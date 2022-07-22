f8a43b29e2616d0153f275d761c415028f47f97c
Alvar <alvarbu@gmail.com>
2022-07-22 11:09:06 -0500

    se agrega etiqueta meta de verificacion de dominio

--- resources/views/layouts/nicxa.blade.php
+++ resources/views/layouts/nicxa.blade.php
@@ -16,6 +16,7 @@
     <meta name=»theme-color» content=»#EB0028»>
     <title>Grupo Nicxa - Nuestra meta eres tú</title>
     <meta name="facebook-domain-verification" content="qhqtbupunltndj8a4lurqu5e5ocshg" />
+    <meta name="google-site-verification" content="q61Lt_yavm_0AcKrsM5i_85AaX7U7w9-hACnBVtjVH8" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     @stack('css')
     <link rel="stylesheet" href="{{asset('/css/font.css')}}">
