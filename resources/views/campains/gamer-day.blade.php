78ab39d9e896988b4afd9e6618fb58092ab54752
Alvar Buenfil <alvarbu@gmail.com>
2024-08-06 11:21:24 -0600

    target blank agregado

--- resources/views/campains/gamer-day.blade.php
+++ resources/views/campains/gamer-day.blade.php
@@ -150,11 +150,11 @@
                         </div>
                         <small class="text-white">Mérida</small>
                         <div class="botones pt-3">
-                            <a href="https://forms.gle/HdTK4ZWNNFrVAeTF9">
+                            <a href="https://forms.gle/HdTK4ZWNNFrVAeTF9" target="blank">
                                 <img src="{{ asset('img/gestion2024/campains/pizzahut/gamer-day/btn-inscripcion.svg') }}"
                                     width="220" alt="boton inscripcion">
                             </a>
-                            <a href="https://maps.app.goo.gl/bmxXrrNWpTuPtfcd9">
+                            <a href="https://maps.app.goo.gl/bmxXrrNWpTuPtfcd9" target="blank">
                                 <img src="{{ asset('img/gestion2024/campains/pizzahut/gamer-day/btn-ubicacion.svg') }}"
                                     width="220" alt="boton inscripcion">
                             </a>
@@ -171,11 +171,11 @@
                         </div>
                         <small class="text-white">Mérida</small>
                         <div class="botones pt-3">
-                            <a href="https://www.start.gg/tournament/liga-hut-gaming-by-pizza-hut-fc-24/details">
+                            <a href="https://www.start.gg/tournament/liga-hut-gaming-by-pizza-hut-fc-24/details" target="blank">
                                 <img src="{{ asset('img/gestion2024/campains/pizzahut/gamer-day/btn-inscripcion.svg') }}"
                                     width="220" alt="boton inscripcion">
                             </a>
-                            <a href="https://maps.app.goo.gl/8SSosJRpT9Z27V7v7">
+                            <a href="https://maps.app.goo.gl/8SSosJRpT9Z27V7v7" target="blank">
                                 <img src="{{ asset('img/gestion2024/campains/pizzahut/gamer-day/btn-ubicacion.svg') }}"
                                     width="220" alt="boton inscripcion">
                             </a>
