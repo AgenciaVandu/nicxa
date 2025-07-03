4b7fb6e34de8ac19896fa1e76dd64903cfb5e1c3
Alvar Buenfil <vandu@MacBook-Air-de-Alvar.local>
2025-07-03 14:22:47 -0600

    correcion navbar + imagen

--- resources/views/layouts/nicxa.blade.php
+++ resources/views/layouts/nicxa.blade.php
@@ -136,7 +136,7 @@
                             <a class="nav-link" href="/unete-a-la-famila-nicxa">Únete a la familia Nicxa <br class="d-block d-sm-block-d-md-none d-lg-none"> <span class="vacantes">¡Vacantes disponibles!</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/beach-on-tour">Beach on tour 2025<br class="d-block d-sm-block-d-md-none d-lg-none"> <span class="vacantes">¡Vacantes disponibles!</span></a>
+                            <a class="nav-link" href="/beach-on-tour">Beach on tour 2025<br class="d-block d-sm-block-d-md-none d-lg-none"></a>
                         </li>
 
                        
